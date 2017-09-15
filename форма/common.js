bcd039d5ebc964fdda544e34dd23b12220d98c86
WebDizainStudio <delhy@mail.ru>
2017-09-15 12:02:38 +0300

    Add files via upload

---
+++ форма/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$(".popup_c").magnificPopup();
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
+});
